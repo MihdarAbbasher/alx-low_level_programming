74adf194519a9fb9002305ec50b9520033099b91
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-08 22:12:59 +0200

    t5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-* main - Prints "with proper grammar, but the outcome is a piece of art,"
+* main - Prints "with proper grammar, but the outcome
+* is a piece of art,"
 * Return: 0
 */
 int main(void)

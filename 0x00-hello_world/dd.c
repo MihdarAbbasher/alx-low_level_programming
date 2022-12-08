c6d16e36ca2d6da2079066df68925a9982d1ebc0
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-08 22:19:36 +0200

    t4

---
+++ 0x00-hello_world/dd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Prints "with proper grammar, but the outcome
+* is a piece of art,"
+* Return: 0
+*/
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}

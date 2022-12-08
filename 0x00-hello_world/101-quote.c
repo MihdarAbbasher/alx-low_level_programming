354a5e83379baaf831aa6209dff1634074f9aac7
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-08 23:14:49 +0200

    t7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Prints "and that piece of art is useful" -
+* Dora Korpar, 2015-10-19"
+* Return: 0
+*/
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

1061d5a0ae5061bd8729ce937b77f03696b4d57e
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-09 22:18:35 +0200

    t8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+* main - print the alphabet
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

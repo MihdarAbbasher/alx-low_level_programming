d2dafd7c005cbd4ac636069e88ec56274963da90
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-09 22:38:15 +0200

    t9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+* main - print digits from 0 - 9
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i); 
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');	
+		}
+}
+	putchar('\n');
+	return (0);
+}

5f0d03db4cad70b8e527d163ae133be3967dd8e5
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-14 18:37:30 +0200

    t10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+* add - print the alphabet
+* @a: int par1
+* @b: int par2
+* Return: int addition result
+*/
+void add(int a, int b)
+{
+	retutn (a + b);
+}

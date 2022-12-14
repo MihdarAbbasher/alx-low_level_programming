6110b23e9fd1d88c90849dc0a58067f09af71350
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-14 21:24:53 +0200

    t9

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+* void print_to_98 - print theto 98
+* @n: int start value
+* Return: void
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		i = -1;
+	}
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		n = n + i;
+	}
+	printf("%d\n", n);
+}

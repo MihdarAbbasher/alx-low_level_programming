4bb8c50379098c34d620169081e340de2b29b60e
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-14 23:02:58 +0200

    t15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+* main - print the alphabet
+* Return: void
+*/
+int main(void)
+{
+	int i;
+	long a, b, tmp;
+
+	a = 1;
+	b = 2;
+	printf("%ld, %ld, ", a, b);
+	while (b <= 4000000)
+	{
+		tmp = a + b;
+		a = b;
+		b = tmp;
+		printf("%ld, ", b);
+	}
+	tmp = a + b;
+	printf("%ld\n", tmp);
+	return (0);
+}

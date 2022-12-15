cd3a2c3ff38779105f658f67a62abfbca2b55116
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-15 20:31:43 +0200

    t2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+* _isdigit - print the alphabet
+* @c: char to check
+* Return: 0
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}

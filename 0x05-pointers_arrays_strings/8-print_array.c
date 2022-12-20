bfdd80bbc753201adb0f05f61e4268cac5f3769e
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-20 23:46:57 +0200

    t8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - check the co
+ * @s: char pointer
+ * @n: int len
+*/
+void print_array(int *s, int n)
+{
+	int i = 0;
+	char c;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		c = *(s + i);
+		printf("%d, ", c);
+	}
+	printf("%d\n", *(s + i));
+}

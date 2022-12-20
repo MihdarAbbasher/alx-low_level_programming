cf453890a7be0d2123efcc6c6632a6a6a6150464
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-20 20:13:34 +0200

    t3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+/**
+ * _puts - check the co
+ * @s: char pointer
+ * Return: int len
+*/
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (1 == 1)
+	{
+		cur = *(s + i)
+		if (cur != '\0')
+		{
+			_putchar(cur + '0');
+			i += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
 void reset_to_98(int *n);
-swap_int(int *a, int *b);
-_strlen(char *s);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
 

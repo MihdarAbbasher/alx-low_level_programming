5f1c3d7e381aa17939b535bd098e48055893ae45
MihdarAbbasher <mihdar.ph@gmail.com>
2022-12-24 21:07:14 +0200

    t9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,22 +2,6 @@
 #include "main.h"
 /* betty style doc for function main goes there */
 /**
- * _size - check the co
- * @s: char pointer
- * Return: int len
-*/
-int _size(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
 * _strcpy - strcpy the str
 * @dest: char pointer
 * @src: char pointer
@@ -25,10 +9,9 @@ int _size(char *s)
 */
 char *_strcpy(char *dest, char *src)
 {
-	int i, ls;
+	int i;
 
-	ls = _size(src);
-	for (i = 0; i < ls && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}

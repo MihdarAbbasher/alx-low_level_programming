b8cb750a69be30006d47da2e9ffe1c9a982ff35e
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-02 23:51:48 +0200

    t2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - check the code
+ * @s: array
+ * @b: char to be written
+ * Return: address
+ */
+char *_strchr(char *s, char b)
+{
+	char i;
+	char *ptr;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == b)
+		{
+			ptr = s + i;
+			break;
+		}
+	}
+	return (ptr);
+}
+

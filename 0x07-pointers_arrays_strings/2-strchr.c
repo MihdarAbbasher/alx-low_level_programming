c2ce7ea8a27a3e75434ee8de5121bf08b4027296
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-03 13:22:11 +0200

    t2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,30 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * _strlen(char *s)- check the co
+ * @s: char pointer
+ * Return: int len
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (1 == 1)
+	{
+		if (*(s + i) != '\0')
+		{
+			i += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (i);
+}
+
+
 /**
  * _strchr - check the code
  * @s: array
@@ -8,11 +33,12 @@
  */
 char *_strchr(char *s, char b)
 {
-	int i;
+	int i, l;
 	char *ptr;
 
+	l = _strlen(s);
 	ptr = NULL;
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; i < l; i++)
 	{
 		if (*(s + i) == b)
 		{

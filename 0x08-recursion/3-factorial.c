5a1ce03484371be2294d0c6729a90ad670a5f9c5
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-05 00:29:05 +0200

    t 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,11 @@
  * @n: number
  * Return: int fact
  */
-int factorial(int n);
+int factorial(int n)
 {
 	if (n == 0)
 	{
-		return(0);
+		return(1);
 	}
 	else if (n < 0)
 	{
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -12,8 +12,16 @@ int main(void)
 {
 
 
-   _print_rev_recursion("\nColton Walker");
-    return (0);
+     int r;
+
+    r = factorial(1);
+    printf("%d\n", r);
+    r = factorial(5);
+    printf("%d\n", r);
+    r = factorial(10);
+    printf("%d\n", r);
+    r = factorial(-1024);
+    printf("%d\n", r);  return (0);
     
     /*
     

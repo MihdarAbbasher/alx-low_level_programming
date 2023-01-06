4aa8d873b1e50458cfe17f5b5bc07808c53d5823
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-07 01:09:28 +0200

    t3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -12,14 +13,14 @@ int main(int argc, char *argv[])
 {
 	int i, a, b;
 	
-	if argc != 3)
+	if (argc < 3)
 	{
 		printf("Error\n");
 	}
 	else
 	{
-		a = atoi(argv[1];
-		b = atoi(argv[2];		
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);		
 		i = a * b;
 		printf("%d\n", i);
 	}    	

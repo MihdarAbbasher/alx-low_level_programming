54a46f899cca187043097f011bd4f5a6686f5042
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-07 00:55:35 +0200

    t3

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,17 +3,18 @@
 
 /**
  * main - check the code
- *
+ * @argc: arg count
+ * @argv: arg vector
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	printf("%s\n", argv[0]);
-	for (i =1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
-	}    	
+	}
 	return (0);
 }

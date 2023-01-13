35da99de73a5f8650c6751169c6d73a93aa59d14
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-13 17:23:59 +0200

    t1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen(char *s)- check the co
+ * @s: char pointer
+ * Return: int len
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (1 == 1)
+	{
+		if (*(s + i) != '\0')
+		{
+			i += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (i);
+}
+
+/**
+ * _strdup - check the code
+ * @str: 
+ * Return: Always ptr or null.
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, j;
+
+	j = _strlen(str);
+	ptr = malloc(sizeof(str) * j * 2);
+	if (ptr != NULL)
+	{
+		for (i = 0; i <= j; i++)
+		{
+			*(ptr + i) = *(str + i);
+		}
+	}
+	if (j == 0)
+	{
+		ptr = NULL;
+	}
+	return (ptr);
+}
--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -32,17 +32,19 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
- char *buffer;
 
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
+  char *s;
+
+    s = _strdup("ALX SE");
+    if (s == NULL)
     {
         printf("failed to allocate memory\n");
         return (1);
     }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
+    printf("%s\n", s);
+    free(s);
     return (0);
+
 }
 
 

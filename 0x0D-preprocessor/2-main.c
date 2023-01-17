487bfecbc3b5f4531a3d86edcbcc8b0d3c5d7b53
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-17 11:25:15 +0200

    t4

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,18 +2,55 @@
 
 /* betty style doc for function main goes there */
 /**
-* _isupper - print the alphabet
-* @c: char to check
+ * _strlen(char *s)- check the co
+ * @s: char pointer
+ * Return: int len
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (1 == 1)
+	{
+		if (*(s + i) != '\0')
+		{
+			i += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (i);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/* betty style doc for function main goes there */
+/**
+* main - print the file name
 * Return: 0
 */
-int _isupper(int c)
+int main(void)
 {
-	if (c >= 'A' && c <= 'Z')
+	char *s;
+	int i, l;
+
+	s = __FILE__;
+	l = _strlen(s);
+	for (i = 0; i < l; i++)
 	{
-		return (1);
+		_putchar(i);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }

c385d4fcc0a9b3f09c905b305aefed13d75fcc16
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-27 18:24:46 +0200

    t4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,14 +4,39 @@
 #include <stdlib.h>
 
 /**
+* free_list_end - check the code
+* @head: list ptr
+* Return: Always list.
+*/
+void free_list_end(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	else
+	{
+		tmp = head;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp = NULL;
+	}
+}
+
+
+/**
 * free_list - check the code
 * @head: list ptr
 * Return: Always list.
 */
 void free_list(list_t *head)
 {
-	if (head != NULL)
+	while (head != NULL)
 	{
-		head = NULL;
+		free_list_end(head);
 	}
 }

df54ff193d4260ddaa473cc476119a1b79f8d4bf
MihdarAbbasher <mihdar.ph@gmail.com>
2023-01-30 21:50:00 +0200

    t4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* free_listint - check the code
+* @head: list ptr
+* Return: Always list.
+*/
+void free_listint(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}

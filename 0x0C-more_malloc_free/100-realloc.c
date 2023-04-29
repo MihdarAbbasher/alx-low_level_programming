8dbd8fd69c3222be4dbb0ce71f8b92227009e565
MihdarAbbasher <mihdar.ph@gmail.com>
2023-04-29 14:56:43 +0200

    t100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,6 +26,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (nptr);
 	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	nptr = malloc(new_size);
 	p = nptr;
 	pp = ptr;

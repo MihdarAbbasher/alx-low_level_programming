03d49fcbfaf4597a3b8897aca32c9efe0b8afd2c
MihdarAbbasher <mihdar.ph@gmail.com>
2023-06-12 22:39:13 +0200

    t1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen(char *s)- check the co
+ * @s: char pointer
+ * Return: int len
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (1 == 1)
+	{
+		if (*(s + i) != '\0')
+		{
+			i += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (i);
+}
+
+/**
+ * _strdup - check the code
+ * @str: str
+ * Return: Always ptr or null.
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, j;
+
+	if (str == NULL)
+	{
+		return (str);
+	}
+
+	else
+	{
+		j = _strlen(str);
+		ptr = malloc(j * 2);
+		if (ptr != NULL)
+		{
+			for (i = 0; i <= j; i++)
+			{
+				*(ptr + i) = *(str + i);
+			}
+		}
+	}
+	return (ptr);
+}

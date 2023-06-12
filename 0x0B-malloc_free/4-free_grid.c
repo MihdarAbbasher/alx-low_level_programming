9374f83f33b856047cf31ca26d82bcc8db8f7ec8
MihdarAbbasher <mihdar.ph@gmail.com>
2023-06-12 22:54:28 +0200

    t4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - check the code
+ * @height: arg vector
+  * @grid: arg vector
+ * Return: Always ptr or null.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (height > 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}

6b930d518a7fbcecf6ddc208d7d4c68fd4bfac3e
MihdarAbbasher <mihdar.ph@gmail.com>
2023-06-15 22:07:20 +0200

    t2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - check the code
+ * @nmemb: arg count
+ * @size: arg count
+ * Return: Always ptr or null.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	char *p;
+	unsigned int i;
+
+	if (size * nmemb == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	p = ptr;
+	if (ptr != NULL)
+	{
+		for (i = 0; i < nmemb * size; i++)
+		{
+			*(p + i) = '\0';
+		}
+		return (ptr);
+	}
+	return (NULL);
+}

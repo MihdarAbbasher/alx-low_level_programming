f9c412eb7ffe98b13bea6a711b1639b161f933d6
MihdarAbbasher <mihdar.ph@gmail.com>
2023-06-19 22:57:07 +0200

    t2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,44 +2,14 @@
 
 /* betty style doc for function main goes there */
 /**
- * _strlen(char *s)- check the co
- * @s: char pointer
- * Return: int len
-*/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (1 == 1)
-	{
-		if (*(s + i) != '\0')
-		{
-			i += 1;
-		}
-		else
-		{
-			break;
-		}
-	}
-	return (i);
-}
-
-/* betty style doc for function main goes there */
-/**
 * main - print the file name
 * Return: 0
 */
 int main(void)
 {
 	char *s;
-	int i, l;
 
 	s = __FILE__;
-	l = _strlen(s);
-	for (i = 0; i < l; i++)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+	printf("%s\n", s);
 	return (0);
 }

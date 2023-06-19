f2403237b52b0ead30d49c2cfa960a918f6ee40c
MihdarAbbasher <mihdar.ph@gmail.com>
2023-06-19 23:29:04 +0200

    t5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+
+/**
+* free_dog - check the code
+* @d: dog object
+* Return: Always nothing.
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		d = NULL;
+	}
+}
+

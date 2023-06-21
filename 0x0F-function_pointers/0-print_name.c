53ea04beefd8c50136fd6aa9220e2c1675c5a196
MihdarAbbasher <mihdar.ph@gmail.com>
2023-06-21 17:36:35 +0200

    t0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+
+/**
+* print_name - check the code
+* @f: dog object
+* @name: dog name
+* Return: Always void.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (*name != '\0')
+	{
+		f(name);
+	}
+}

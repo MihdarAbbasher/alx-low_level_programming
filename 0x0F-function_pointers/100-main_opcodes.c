29a5900b4f89aa49998b469fc4dae1c5b8b3f376
MihdarAbbasher <mihdar.ph@gmail.com>
2023-06-24 22:15:43 +0200

    t100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,17 +1,37 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(int ac, argv[])
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
+int main(int argc, char *argv[])
 {
-	if (ac != 3)
+	int i, n;
+
+
+	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(98);
-	}    
-	    
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
-

cc985aacb8bf897cf051d42b2fdff896564f310c
MihdarAbbasher <mihdar.ph@gmail.com>
2023-07-01 16:52:56 +0200

    t100

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,4 +22,6 @@ unsigned int _strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void first(void);
+
 #endif

c792bebc44f2f8ccfe564809bda25ba88b59a1fa
MihdarAbbasher <mihdar.ph@gmail.com>
2023-07-10 21:32:01 +0200

    t1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+  * _strlen - get len of str
+  * @s: the str
+  *
+  * Return: len of str
+  */
+int _strlen(char *s)
+{
+	int len;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
  * create_file - write a file
  * @filename: the file to write.
  * @text_content: the content to write
@@ -8,23 +23,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, _write, i;
+	int len, fd, writeVal;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
-	if (fd == -1)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		while (text_content[i])
-			i++;
-		_write = write(fd, text_content, i);
-		if (_write == -1)
-			return (-1);
+		close(fd);
+		return (1);
 	}
-	close(fd);
+	len = _strlen(text_content);
+	writeVal = write(fd, text_content, len);
+	if (writeVal < 0 || writeVal != len)
+		return (-1);
 	return (1);
 }
+

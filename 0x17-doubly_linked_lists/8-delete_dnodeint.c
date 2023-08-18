fc396b6bf5d01c74ab5923b1afd5bc48956dfafa
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-18 21:38:14 +0200

    t8 update style 3

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -42,7 +42,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = (*head)->next;
 		(*head)->prev = NULL;
 		free(tmp);
-		return(1);
+		return (1);
 	}
 	if (index == len - 1)
 	{

7bd7df186439aa7be2d3bd85c33a9e72c1d830d0
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-18 11:49:56 +0200

    t6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * int sum_dlistint - sum.
+ * @head: head of dlinked list
+ * Return: sum of data.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int res;
+	dlistint_t *node;
+
+	node = head;
+	res = 0;
+	while (node)
+	{
+		res += node->n;
+		node = node->next;
+	}
+	return (res);
+}

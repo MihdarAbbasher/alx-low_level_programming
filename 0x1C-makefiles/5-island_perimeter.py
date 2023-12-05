3f9dbdaa6cd4f7bc0b7fd0cb41e698fbc324582c
MihdarAbbasher <mihdar.ph@gmail.com>
2023-12-04 23:26:12 -0800

    t5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# module for island perimeter
+
+def island_perimeter(grid):
+    """return the perimeter of grid"""
+    res = 0
+
+    if not grid:
+        return 0
+    h = len(grid)
+    w = len(grid[0])
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j]:
+                if i <= 0 or not grid[i - 1][j]:
+                    res += 1
+                if j <= 0 or not grid[i][j - 1]:
+                    res += 1
+                if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+                    res += 1
+                if i >= len(grid) - 1 or not grid[i + 1][j]:
+                    res += 1
+    return res
